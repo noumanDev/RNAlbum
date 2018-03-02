c12628f9b0a02ad0deb631de6c76838e4287790c
nouman nawaz <noumannawaz291@gmail.com>
2018-03-02 18:05:32 +0500

    initial commit

---
+++ App.js
@@ -0,0 +1,73 @@
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  * @flow
+//  */
+
+// import React, { Component } from 'react';
+// import {
+//   Platform,
+//   StyleSheet,
+//   Text,
+//   View
+// } from 'react-native';
+
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' +
+//     'Cmd+D or shake for dev menu',
+//   android: 'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
+
+// type Props = {};
+// export default class App extends Component<Props> {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>
+//           Welcome to React Native!
+//         </Text>
+//         <Text style={styles.instructions}>
+//           To get started, edit App.js
+//         </Text>
+//         <Text style={styles.instructions}>
+//           {instructions}
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
+
+
+// import a library to help create a component
+// import React from 'react';
+// import { Text } from 'react-native';
+
+// //create a component
+// export default const App = () => (
+//     <Text>some Text</Text>
+//   );
+
+
+// //render it to the device
+// //AppRegistry.registerComponent('albums', () => App);
+
---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { View, Text, Image, Linking } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
+
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image, image, url } = album;
+    const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles;
+
+    return (
+        <Card>
+            <CardSection>
+                <View style={thumbnailContainerStyle}>
+                    <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+
+            </CardSection>
+            <CardSection>
+                <Image style={imageStyle} source={{ uri: 'https://cdn.pixabay.com/photo/2016/12/01/07/30/music-1874621_960_720.jpg' }} />
+            </CardSection>
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)} >
+                Buy Now
+                </Button>
+            </CardSection>
+        </Card>
+    );
+
+
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
+};
+
+export default AlbumDetail;
